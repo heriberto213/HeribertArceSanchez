4106f5fad85d84a7cbc01a8eff8fb96b18063d18
heriberto213 <Hery_Berto213Arce@hotmail.com>
2019-02-12 13:29:42 -0600

    new

---
+++ Password.java
@@ -0,0 +1,33 @@
+public class Password {
+    //atributos 
+    sss
+    public String contraseña;
+    public Password (int logitud ) 
+
+    {
+        this logitud=logitud;
+        contraseña=generaPassword(),
+        System.out.println("la contraseña ingresada es "+contraseña );
+        if(esFuerte()==true)
+        {
+            System.out.println ("la contraseña ingresada es fuerte ");
+            
+            }
+            else
+            {
+                System.OUT.println("la contraseña ingresada es debil");
+
+            }
+            pu String generaPassword()
+            {
+                String Password="";
+                for(int i=0;i<longuitud;i++)
+                {
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ pasword.java
@@ -0,0 +1,44 @@
+public class pasword{
+    //atributos
+    private int longuitud;
+   
+    double Contraseña; 
+     double length;
+     // metodos 
+     public void calLonguitud(){ 
+    Longuitud = math
+    
+
+    }
+    public void calContraseña(){
+    Contraseña = math
+     }
+     publi double getLonguitud(){
+         return Longuitud;
+     }
+     publi double getContraseña(){
+         return Contraseña;
+
+     }
+     public void setLength(double length){
+    }  
+     public static void main(String[] args) {
+          } 
+    pasword pasword1 = new  pasword  ();
+
+     System.out.println("la contraseña ingresada es "+contraseña );
+        if(esFuerte()==true)
+        {
+            System.out.println ("la contraseña ingresada es fuerte ");
+            
+            }
+            else
+            {
+                System.OUT.println("la contraseña ingresada es debil");
+
+            }
+            pu String generaPassword()
+            {
+                String Password="";
+            
+}
